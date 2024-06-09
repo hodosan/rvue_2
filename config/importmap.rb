406583921aab84302bf51a5d328c18d52b3ad788
N.Hodoshima <nobu@example.com>
2024-06-09 08:19:15 +0000

    end of 4-2-1

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "vue", to: "https://ga.jspm.io/npm:vue@3.4.27/dist/vue.esm-browser.js", preload: true
+pin_all_from "app/javascript/components", under: "components"
